981f970babe4a09a7aea944e0d4413ae42341c1f
amolmt <amoltangade@dbatu.ac.in>
2020-01-29 23:10:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-8gw59t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-8gw59t)
---
+++ index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+       
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
